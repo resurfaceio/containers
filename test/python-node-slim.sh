2efe4d9bebc885c968ea0637744f49246af6ef4f
Ramón Márquez <ramn.marquez@gmail.com>
2022-01-07 19:17:56 -0400

    chore(test): move -slim suffix to tag

--- test/python-node-slim.sh
+++ test/python-node-slim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -f python-node-slim.dockerfile -t python-node-slim:$1 --no-cache .
-docker tag python-node-slim:$1 resurfaceio/python-node-slim:$1
-docker push resurfaceio/python-node-slim:$1
+docker build -f python-node-slim.dockerfile -t python-node:$1-slim --no-cache .
+docker tag python-node:$1-slim resurfaceio/python-node:$1-slim
+docker push resurfaceio/python-node:$1-slim
